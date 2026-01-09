dfde92db3f60ee3d7b6d6731b029991181944593
Brunno dos Santos <brunno.dossantosleite@remote.com>
2026-01-09 17:57:56 -0300

    Fix: Use Burrito.Util.Args.argv() instead of System.argv() for command line arguments

--- lib/construir_escala/application.ex
+++ lib/construir_escala/application.ex
@@ -5,7 +5,8 @@ defmodule ConstruirEscala.Application do
   def start(_type, _args) do
     # For a CLI tool, we don't need a supervision tree
     # We'll just run the CLI and exit
-    args = System.argv()
+    # Use Burrito's Args module to get command line arguments when wrapped
+    args = Burrito.Util.Args.argv()
     ConstruirEscala.CLI.main(args)
     System.halt(0)
   end
